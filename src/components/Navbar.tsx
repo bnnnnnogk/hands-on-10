1d30e0af3ec6a6da2e8b7e7ee864b4ba3514e069
Konz <konz@Konzs-MacBook-Air.local>
2023-10-11 13:15:32 +0700

    add style to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom"
+import styles from "./Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <header>
+    <header className={styles.header__navbar}>
       <Link to="/">
-        <div>
+        <div className={styles.container__logo}>
           <img
+            className={styles.logo__img}
             src="https://learnhub.thanayut.in.th/logo.svg"
             alt="LearnHub logo"
           />
           <span>LearnHub</span>
         </div>
       </Link>
-      <nav>
+      <nav className={styles.container__navbar}>
         <Link to="/">Login</Link>
         <Link to="/">Register</Link>
       </nav>
