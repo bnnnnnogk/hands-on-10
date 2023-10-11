937fc2eef31eff8607d8cc2db9bc0fa7eab89085
Konz <konz@Konzs-MacBook-Air.local>
2023-10-11 13:49:43 +0700

    add css to banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,10 @@
+import styles from "./Banner.module.css"
+
 const Banner = () => {
   return (
-    <section>
-      <h1>LearnHub</h1>
-      <h2>Hub for Educational Videos</h2>
+    <section className={styles.container__banner}>
+      <h1 className={styles.main__title}>LearnHub</h1>
+      <h2 className={styles.sub_title}>Hub for Educational Videos</h2>
     </section>
   )
 }
