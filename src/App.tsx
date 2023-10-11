2cdff6e9e61075b86d1fe59c262987125a0225cf
Konz <konz@Konzs-MacBook-Air.local>
2023-10-11 13:55:06 +0700

    add route mainpage to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
-import FullPages from "./template/Fullpages"
+import FullPages from "./template/FullPages"
+import MainPages from "./pages/MainPages"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<FullPages />} />
+      <Route index element={<MainPages />} />
     </Routes>
   )
 }
